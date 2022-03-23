1219325a0ae98ba92be78fb038fe285b565a3425
Sofian <sofianmustafa17@gmail.com>
2022-03-23 00:10:54 -0600

    business profile with data on 'submit' form

--- public/Clients/businessOwnerProfileClient.js
+++ public/Clients/businessOwnerProfileClient.js
@@ -1,10 +1,13 @@
 let businessData;
 
+// request data from the server based on the id in the url
 async function fetchProfile() {
     let response = await fetch('/business-profile/generate/' + window.location.href.split('/')[4]);
     let businessData = await response.json();
-    console.log(data);
+    console.log(businessData);
 }
 $(document).ready(function() {
     fetchProfile();
+
+
 });
