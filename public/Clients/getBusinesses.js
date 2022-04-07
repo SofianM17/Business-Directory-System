90f959561c8db09eee26162caa9d04abbd2b99a2
Sofian <sofianmustafa17@gmail.com>
2022-04-06 22:20:40 -0600

    Business owner dashboard styles

--- public/Clients/getBusinesses.js
+++ public/Clients/getBusinesses.js
@@ -1,16 +1,16 @@
-$(document).ready(async function () {
+$(document).ready(async function() {
     let results = await fetchResults();
 
-    for(i =0; i<results.length; i++) {
+    for (i = 0; i < results.length; i++) {
         var li = document.createElement('li');
         var a = document.createElement('a');
         var img = document.createElement('img');
         var businessID = results[i]._id;
-        console.log(businessID);
+        //console.log(businessID);
         a.setAttribute('href', "/business-profile-owner/" + businessID);
         a.setAttribute('class', "btn btn-outline-dark");
-        a.innerHTML = results[i].name;
-        img.setAttribute('src', "../Resources/store.jpg");
+        a.innerHTML = '<h6>' + results[i].name + '</h6>';
+        img.setAttribute('src', "../Resources/store.png");
         a.appendChild(img);
         li.appendChild(a);
         console.log(a.href);
@@ -19,7 +19,7 @@ $(document).ready(async function () {
 });
 
 
-async function fetchResults(){
+async function fetchResults() {
     let response = await fetch("/businesses/" + window.location.href.split("/")[4]);
     let results = await response.json();
     return results;
